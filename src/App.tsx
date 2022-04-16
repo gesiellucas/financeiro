a530cd63fffac2f09b9b880023fe3796c144cad4
Gesiel Lucas <gesiel.lucas@hotmail.com>
2022-04-16 08:51:46 -0300

    bug: Error to create api from button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button, TextField, Box } from '@mui/material';
+
+export default function App(){
+    const url = 'htttp://localhost:8000/api/amount2';
+
+    const [interest, setInterest] = useState(null);
+    const [response, setResponse] = useState(null);
+    const [value, setValue] = useState(0);
+
+    useEffect( ()=> {
+        console.log(response)
+    }, [response]);
+
+    function chamarApi(e){
+        axios
+            .get(url)
+            .then( res =>{
+                setResponse(res)
+            })
+            .catch( err => {
+                console.log(err)
+            })
+    }
+
+    const handleClick = (string: event) => {
+        setInterest(event.target.value);
+    }
+
+    return(
+        <>
+        <Box component="section" sx={{display: 'flex', justifyContent: 'space-around'}}>
+            <TextField label="R$" onChange={handleClick} value={interest} variant="outlined"/>
+            <Button onClick={() => chamarApi(interest)} size="large" variant="contained" color="warning">Gasto</Button>
+            <Button size="large" variant="contained">Ganho</Button> 
+        </Box>
+
+        <p> {value} </p>
+        </>
+    )
 }
-
-export default App;
